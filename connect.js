6bd69d4241533e3a29476bbd887e1b65f44bdb17
poojamahendiren <poojamahendiren99@gmail.com>
2023-05-02 13:35:58 +0530

    initial commit

---
+++ connect.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+
+
+module.exports = {
+
+    async connect() {
+        selectedDb: { };
+        try {
+            const client = await MongoClient.connect(process.env.MONGODB_URL);
+            this.selectedDb = client.db("guvi");
+            console.log("DB connection established");
+            //console.log(this.selectedDb);
+        } catch (err) {
+            console.error(err)
+        }
+    }
+};
